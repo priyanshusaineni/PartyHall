8a8e739b190bf4f58c70ffecd56791506cd3dca7
shivaramakrishnamarepalli <marepallishivaramakrishna@gmail.com>
2024-05-22 17:22:08 +0530

    "Hall bookings added"

--- Controllers/superadmin.controller.js
+++ Controllers/superadmin.controller.js
@@ -1,55 +1,67 @@
-const SuperAdminModel = require('../Models/superAdmin.model')
-const HallModel = require('../Models/hall.model')
-const AdminModel = require('../Models/admin.model')
-const UserModel = require('../Models/user.model')
-const { addHall } = require('../Controllers/hall.controller')
+const SuperAdminModel = require("../Models/superAdmin.model");
+const HallModel = require("../Models/hall.model");
+const AdminModel = require("../Models/admin.model");
+const UserModel = require("../Models/user.model");
+const BookingsModel = require("../Models/booking.model");
+const { addHall } = require("../Controllers/hall.controller");
+const { isSuperAdmin } = require("./auth.controller");
 
 async function deleteAdmin(req, res) {
-  const { id } = req.params
-  console.log(id)
-  const admin = await AdminModel.findOneAndDelete({ admin_id: id })
+  const { id } = req.params;
+  console.log(id);
+  const admin = await AdminModel.findOneAndDelete({ admin_id: id });
   if (!admin) {
-    res.status(200).json({ message: 'Error! Invalid Admin ID' })
+    res.status(200).json({ message: "Error! Invalid Admin ID" });
   } else {
-    res.status(200).json({ message: 'Admin Deleted With Given Id' })
+    res.status(200).json({ message: "Admin Deleted With Given Id" });
   }
 }
 async function deleteUser(req, res) {
-  const { id } = req.params
+  const { id } = req.params;
   // console.log(user_id)
-  const user = await UserModel.findOneAndDelete({ user_id: id })
+  const user = await UserModel.findOneAndDelete({ user_id: id });
   if (!user) {
-    res.status(200).json({ message: 'Error! Invalid User ID' })
+    res.status(200).json({ message: "Error! Invalid User ID" });
   } else {
-    res.status(200).json({ message: 'User Deleted With Given Id' })
+    res.status(200).json({ message: "User Deleted With Given Id" });
   }
 }
-async function getBookings() {}
+
+async function getBookings(req, res) {
+  if (!isSuperAdmin()) {
+    res.status(404).send("Access Denied");
+    return;
+  }
+  const bookings = await BookingsModel.findOne({});
+  const bookingRecords = bookings.bookingRecords;
+  console.log(bookingRecords);
+  res.status(200).send(bookingRecords);
+}
 
 async function acceptRequest(req, res) {
-  const { hall_id } = req.body
-  const superAdmin = await SuperAdminModel.findOne({})
-  let reqs = superAdmin.requests_pending_to_add_hall
-  let hall1 = await HallModel.findOne({ hall_id: hall_id })
+  const { hall_id } = req.body;
+  const superAdmin = await SuperAdminModel.findOne({});
+  let reqs = superAdmin.requests_pending_to_add_hall;
+  let hall1 = await HallModel.findOne({ hall_id: hall_id });
   if (hall1 != null) {
-    res.status(404).send('Hall id is taken!')
-    return
+    res.status(404).send("Hall id is taken!");
+    return;
   }
 
-  hall1 = reqs.find((hall) => hall.hall_id == hall_id)
+  hall1 = reqs.find((hall) => hall.hall_id == hall_id);
 
   if (!hall1) {
-    res.status(404).send('NO hall found!')
-    return
+    res.status(404).send("NO hall found!");
+    return;
   }
-  console.log(hall1)
+  console.log(hall1);
 
-  reqs = reqs.filter((hall) => hall.hall_id != hall_id)
+  reqs = reqs.filter((hall) => hall.hall_id != hall_id);
   const requsts = await SuperAdminModel.findOneAndUpdate({
     requests_pending_to_add_hall: reqs,
-  })
+  });
 
-  await addHall(hall1, req, res)
+  await addHall(hall1, req, res);
 }
 
 module.exports = {
@@ -57,4 +69,4 @@ module.exports = {
   deleteUser,
   getBookings,
   acceptRequest,
-}
+};
--- Controllers/user.controller.js
+++ Controllers/user.controller.js
@@ -1,47 +1,145 @@
 //user controller
-const BookingsModel = require('../Models/booking.model')
-const UserModel = require('../Models/user.model')
+const BookingsModel = require("../Models/booking.model");
+const UserModel = require("../Models/user.model");
+const { getBookings } = require("../Controllers/superadmin.controller");
 
 async function getUserBookings(req, res) {
-  let { id } = req.params
-  let user_booking = await BookingsModel.find({ user_id: id })
+  let { id } = req.params;
+  let user_booking = await BookingsModel.find({ user_id: id });
   if (user_booking) {
-    res.status(200).json(user_booking)
+    res.status(200).json(user_booking);
   } else {
-    res.status(404).json({ message: 'No Bookings done by user' })
+    res.status(404).json({ message: "No Bookings done by user" });
   }
 }
 
-async function bookHall() {}
+function isPaymentDone() {
+  return true;
+}
+
+function generateRandomId() {
+  function generateRandomString(length) {
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let randomString = "";
+    for (let i = 0; i < length; i++) {
+      randomString += characters.charAt(
+        Math.floor(Math.random() * characters.length)
+      );
+    }
+    return randomString;
+  }
+
+  const part1 = generateRandomString(4);
+  const part2 = generateRandomString(4);
+  const part3 = generateRandomString(4);
+
+  const randomId = `${part1}-${part2}-${part3}`;
+
+  return randomId;
+}
+
+async function bookHall(req, res) {
+  if (!isPaymentDone) {
+    res.status(404).send("Payment is pending!");
+    return;
+  }
+  const { id } = req.params;
+  const {
+    user_id,
+    booked_date,
+    duration_hours,
+    cancellation_buffer_days,
+    paid_amount_as_rent,
+    number_of_guests,
+    event_type,
+    catering_amount_paid,
+    total_amount_paid,
+  } = req.body;
+
+  let booking_id = generateRandomId();
+  const bookings = await BookingsModel.findOne({});
+  let bookingRecords = bookings.bookingRecords;
+  console.log(bookings);
+
+  let repeatedId = bookingRecords.filter(
+    (bookingRecord) => bookingRecord.booking_id === booking_id
+  );
+  while (repeatedId.length !== 0) {
+    booking_id = generateRandomId();
+    repeatedId = bookingRecords.filter(
+      (bookingRecord) => bookingRecord.booking_id === booking_id
+    );
+  }
+
+  if (
+    booking_id ||
+    id ||
+    user_id ||
+    booked_date ||
+    duration_hours ||
+    cancellation_buffer_days ||
+    paid_amount_as_rent ||
+    number_of_guests ||
+    event_type ||
+    catering_amount_paid ||
+    total_amount_paid
+  ) {
+    let newRecord = {
+      booking_id,
+      user_id,
+      hall_id: id,
+      booked_date,
+      duration_hours,
+      cancellation_buffer_days,
+      paid_amount_as_rent,
+      number_of_guests,
+      event_type,
+      catering_amount_paid,
+      total_amount_paid,
+    };
+    console.log(newRecord);
+    bookingRecords.push(newRecord);
+
+    console.log(bookingRecords);
+
+    const booking = await BookingsModel.updateOne({
+      bookingRecords: bookingRecords,
+    });
+    res.status(200).json(booking);
+    return;
+  }
+
+  res.status(200).send("Hall is booked!");
+}
 
 async function getProfile(req, res) {
-  let { id } = req.params
-  const user = await UserModel.findOne({ user_id: id })
+  let { id } = req.params;
+  const user = await UserModel.findOne({ user_id: id });
   if (!user) {
-    res.status(404).json({ message: 'User not found OR Invalid User' })
+    res.status(404).json({ message: "User not found OR Invalid User" });
   } else {
-    res.status(200).json(user)
+    res.status(200).json(user);
   }
 }
 
 async function editProfile(req, res) {
-  let { id } = req.params
-  const user = await UserModel.findOneAndUpdate({ user_id: id }, req.body)
+  let { id } = req.params;
+  const user = await UserModel.findOneAndUpdate({ user_id: id }, req.body);
   if (!user) {
-    res.status(404).json({ message: 'User not found' })
+    res.status(404).json({ message: "User not found" });
   } else {
-    const user = await UserModel.findOne({ user_id: id })
-    res.status(200).json(user)
+    const user = await UserModel.findOne({ user_id: id });
+    res.status(200).json(user);
   }
 }
 
 async function getBookingsCalendar(req, res) {
-  const calendar = BookingsModel.calendar
+  const calendar = BookingsModel.calendar;
   if (calendar) {
-    res.status(200).send(calendar)
-    return
+    res.status(200).send(calendar);
+    return;
   }
-  res.status(404).send('No calendar found!')
+  res.status(404).send("No calendar found!");
 }
 
 module.exports = {
@@ -50,4 +148,4 @@ module.exports = {
   getProfile,
   editProfile,
   getBookingsCalendar,
-}
+};
