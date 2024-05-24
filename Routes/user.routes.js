3a3a2071fdab4e296bd33ca52211cd52e77845bc
shivaramakrishnamarepalli <marepallishivaramakrishna@gmail.com>
2024-05-24 12:27:04 +0530

    "Edited routes in user"

--- Routes/user.routes.js
+++ Routes/user.routes.js
@@ -1,5 +1,7 @@
-const express = require('express')
-const { addNewUser, userLogin } = require('../Controllers/auth.controller')
+const express = require("express");
+const { addNewUser, userLogin } = require("../Controllers/auth.controller");
+const multer = require("multer");
+
 const {
   getUserBookings,
   bookHall,
@@ -10,24 +12,35 @@ const {
   deleteReview,
   editReview,
   getReview,
-} = require('../Controllers/user.controller')
-const { getHalls, getHallsById } = require('../Controllers/hall.controller')
-const router = express.Router()
+} = require("../Controllers/user.controller");
+const {
+  getHalls,
+  getHallsById,
+  uploadHallImage,
+  getHallImage,
+  updateHallImage,
+} = require("../Controllers/hall.controller");
+const router = express.Router();
 
-router.use(express.json())
+router.use(express.json());
 
-router.post('/login', userLogin)
-router.post('/signup', addNewUser)
-router.get('/bookings', getUserBookings)
-router.get('/getHalls', getHalls)
-router.get('/getAvailableHalls/:date', getAvailableHalls)
-router.get('/getHalls/:id', getHallsById)
-router.post('/bookHall/:id', bookHall)
-router.get('/getProfile/:id', getProfile)
-router.put('/editProfile/:id', editProfile)
+router.post("/login", userLogin);
+router.post("/signup", addNewUser);
+router.get("/bookings", getUserBookings);
+router.get("/getHalls", getHalls);
+router.get("/getAvailableHalls/:date", getAvailableHalls);
+router.get("/getHalls/:id", getHallsById);
+router.post("/bookHall/:id", bookHall);
+router.get("/getProfile/:id", getProfile);
+router.put("/editProfile/:id", editProfile);
 
-router.post('/review', addReview)
-router.delete('/review/:id', deleteReview)
-router.put('/review/:id', editReview)
-router.get('/review/:id', getReview)
-module.exports = router
+router.post("/review", addReview);
+router.delete("/review/:id", deleteReview);
+router.put("/review/:id", editReview);
+router.get("/review/:id", getReview);
+
+const upload = multer({ storage: multer.memoryStorage() });
+router.post("/uploadImage/:id", upload.single("image"), uploadHallImage);
+router.post("/updateImage/:id", upload.single("image"), updateHallImage);
+router.get("/image/:id", getHallImage);
+module.exports = router;
--- index.js
+++ index.js
@@ -1,22 +1,29 @@
-const express = require('express')
-const app = express()
-const mongoose = require('mongoose')
-const superAdminRoutes = require('./Routes/superadmin.routes')
-const adminRoutes = require('./Routes/admin.routes')
-const userRoutes = require('./Routes/user.routes')
+const express = require("express");
+const app = express();
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+
+const mongoose = require("mongoose");
+const superAdminRoutes = require("./Routes/superadmin.routes");
+const adminRoutes = require("./Routes/admin.routes");
+const userRoutes = require("./Routes/user.routes");
 mongoose
-  .connect('mongodb://localhost:27017/PartyHall')
+  .connect("mongodb://localhost:27017/PartyHall", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
   .then(() => {
-    console.log('successful')
+    console.log("successful");
   })
   .catch(() => {
-    console.log('Connection failed')
-  })
+    console.log("Connection failed");
+  });
 
-app.use('/api/super', superAdminRoutes)
-app.use('/api/admin', adminRoutes)
-app.use('/api/user', userRoutes)
+app.use("/api/super", superAdminRoutes);
+app.use("/api/admin", adminRoutes);
+app.use("/api/user", userRoutes);
 
 app.listen(3006, () => {
-  console.log('Server Running ')
-})
+  console.log("Server Running ");
+});
