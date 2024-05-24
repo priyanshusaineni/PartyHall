de7fad4d29b1079cf1b44a4c0c71c145b7db5ae1
priyanshusaineni <priyanshusaineni2004@gmail.com>
2024-05-24 12:33:00 +0530

    added token verification in user.routes

--- Routes/user.routes.js
+++ Routes/user.routes.js
@@ -1,6 +1,10 @@
-const express = require("express");
-const { addNewUser, userLogin } = require("../Controllers/auth.controller");
-const multer = require("multer");
+const express = require('express')
+const {
+  addNewUser,
+  userLogin,
+  verifyToken,
+} = require('../Controllers/auth.controller')
+const multer = require('multer')
 
 const {
   getUserBookings,
@@ -12,35 +16,35 @@ const {
   deleteReview,
   editReview,
   getReview,
-} = require("../Controllers/user.controller");
+} = require('../Controllers/user.controller')
 const {
   getHalls,
   getHallsById,
   uploadHallImage,
   getHallImage,
   updateHallImage,
-} = require("../Controllers/hall.controller");
-const router = express.Router();
+} = require('../Controllers/hall.controller')
+const router = express.Router()
 
-router.use(express.json());
+router.use(express.json())
 
-router.post("/login", userLogin);
-router.post("/signup", addNewUser);
-router.get("/bookings", getUserBookings);
-router.get("/getHalls", getHalls);
-router.get("/getAvailableHalls/:date", getAvailableHalls);
-router.get("/getHalls/:id", getHallsById);
-router.post("/bookHall/:id", bookHall);
-router.get("/getProfile/:id", getProfile);
-router.put("/editProfile/:id", editProfile);
+router.post('/login', userLogin)
+router.post('/signup', addNewUser)
+router.get('/bookings', verifyToken, getUserBookings)
+router.get('/getHalls', getHalls)
+router.get('/getAvailableHalls/:date', getAvailableHalls)
+router.get('/getHalls/:id', verifyToken, getHallsById)
+router.post('/bookHall/:id', verifyToken, bookHall)
+router.get('/getProfile/:id', verifyToken, getProfile)
+router.put('/editProfile/:id', verifyToken, editProfile)
 
-router.post("/review", addReview);
-router.delete("/review/:id", deleteReview);
-router.put("/review/:id", editReview);
-router.get("/review/:id", getReview);
+router.post('/review', verifyToken, addReview)
+router.delete('/review/:id', verifyToken, deleteReview)
+router.put('/review/:id', verifyToken, editReview)
+router.get('/review/:id', verifyToken, getReview)
 
-const upload = multer({ storage: multer.memoryStorage() });
-router.post("/uploadImage/:id", upload.single("image"), uploadHallImage);
-router.post("/updateImage/:id", upload.single("image"), updateHallImage);
-router.get("/image/:id", getHallImage);
-module.exports = router;
+const upload = multer({ storage: multer.memoryStorage() })
+router.post('/uploadImage/:id', upload.single('image'), uploadHallImage)
+router.post('/updateImage/:id', upload.single('image'), updateHallImage)
+router.get('/image/:id', getHallImage)
+module.exports = router
