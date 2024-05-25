3328a9ece4f99433ccc84f68cc3c96f4d45d7b78
shivaramakrishnamarepalli <marepallishivaramakrishna@gmail.com>
2024-05-25 15:07:32 +0530

    usercontroller update

--- Controllers/user.controller.js
+++ Controllers/user.controller.js
@@ -1,60 +1,60 @@
 //user controller
-const jwt = require('jsonwebtoken')
-const HallModel = require('../Models/hall.model')
-const BookingsModel = require('../Models/booking.model')
-const UserModel = require('../Models/user.model')
-const ReviewModel = require('../Models/userreview.model')
+const jwt = require("jsonwebtoken");
+const HallModel = require("../Models/hall.model");
+const BookingsModel = require("../Models/booking.model");
+const UserModel = require("../Models/user.model");
+const ReviewModel = require("../Models/userreview.model");
 async function getUserBookings(req, res) {
-  let err = await verify1(req)
+  let err = await verify1(req);
   if (err == 1) {
-    res.status(404).send({ message: 'Token mismatch' })
-    return
+    res.status(404).send({ message: "Token mismatch" });
+    return;
   }
-  let { id } = req.params
-  let user_booking = await BookingsModel.find({ user_id: id })
+  let { id } = req.params;
+  let user_booking = await BookingsModel.find({ user_id: id });
   if (user_booking) {
-    res.status(200).json(user_booking)
+    res.status(200).json(user_booking);
   } else {
-    res.status(404).json({ message: 'No Bookings done by user' })
+    res.status(404).json({ message: "No Bookings done by user" });
   }
 }
 
 function isPaymentDone() {
-  return true
+  return true;
 }
 
 function generateRandomId() {
   function generateRandomString(length) {
-    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
-    let randomString = ''
+    const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let randomString = "";
     for (let i = 0; i < length; i++) {
       randomString += characters.charAt(
         Math.floor(Math.random() * characters.length)
-      )
+      );
     }
-    return randomString
+    return randomString;
   }
 
-  const part1 = generateRandomString(4)
-  const part2 = generateRandomString(4)
-  const part3 = generateRandomString(4)
+  const part1 = generateRandomString(4);
+  const part2 = generateRandomString(4);
+  const part3 = generateRandomString(4);
 
-  const randomId = `${part1}-${part2}-${part3}`
+  const randomId = `${part1}-${part2}-${part3}`;
 
-  return randomId
+  return randomId;
 }
 
 async function bookHall(req, res) {
-  let err = await verify1(req)
+  let err = await verify1(req);
   if (err == 1) {
-    res.status(404).send({ message: 'Token mismatch' })
-    return
+    res.status(404).send({ message: "Token mismatch" });
+    return;
   }
   if (!isPaymentDone) {
-    res.status(404).send('Payment is pending!')
-    return
+    res.status(404).send("Payment is pending!");
+    return;
   }
-  const { id } = req.params
+  const { id } = req.params;
   const {
     user_id,
     booked_date,
@@ -65,61 +65,61 @@ async function bookHall(req, res) {
     event_type,
     // catering_amount_paid,
     // total_amount_paid,
-  } = req.body
+  } = req.body;
 
-  console.log(paid_amount_as_rent)
-  
-  let booking_id = generateRandomId()
-  const bookings = await BookingsModel.findOne({})
-  let bookingRecords = []
-  if (bookings) bookingRecords = bookings.bookingRecords
-  
+  console.log(paid_amount_as_rent);
+
+  let booking_id = generateRandomId();
+  const bookings = await BookingsModel.findOne({});
+  let bookingRecords = [];
+  if (bookings) bookingRecords = bookings.bookingRecords;
+
   let repeatedId = bookingRecords.filter(
     (bookingRecord) => bookingRecord.booking_id === booking_id
-  )
+  );
   while (repeatedId.length !== 0) {
-    booking_id = generateRandomId()
+    booking_id = generateRandomId();
     repeatedId = bookingRecords.filter(
       (bookingRecord) => bookingRecord.booking_id === booking_id
-    )
+    );
   }
-  
+
   //check if hall id ,user id is valid or not
-  const halls = await HallModel.find({})
-  const users = await UserModel.find({})
-  const hallExists = halls.find((hall) => hall.hall_id === id)
-  const userExists = users.find((user) => user.user_id === user_id)
-  
+  const halls = await HallModel.find({});
+  const users = await UserModel.find({});
+  const hallExists = halls.find((hall) => hall.hall_id === id);
+  const userExists = users.find((user) => user.user_id === user_id);
+
   if (hallExists && userExists) {
-    console.log('Both hall and user exist.')
+    console.log("Both hall and user exist.");
   } else {
-    console.log("token problem")
-    res.status(404).send('Hall or user does not exist!')
-    return
+    console.log("token problem");
+    res.status(404).send("Hall or user does not exist!");
+    return;
   }
-  let cal = []
-  if (bookings) cal = bookings.calendar
+  let cal = [];
+  if (bookings) cal = bookings.calendar;
   //calendar is an array which has dates array
   //check if the cal already has a entry on the booked_date
-  const bookedDate = new Date(booked_date)
+  const bookedDate = new Date(booked_date);
   let particularDate = cal.find(
     (dateObj) => dateObj.date.getTime() === bookedDate.getTime()
-  )
+  );
 
-  let halls_booked = []
-  if (particularDate) halls_booked = particularDate.halls_booked
-  console.log(particularDate,"Particular date")
+  let halls_booked = [];
+  if (particularDate) halls_booked = particularDate.halls_booked;
+  console.log(particularDate, "Particular date");
 
-  let contains = halls_booked.find((hall_id) => hall_id === id)
+  let contains = halls_booked.find((hall_id) => hall_id === id);
   if (contains) {
-    res.status(404).send('The date is already booked for this hall!')
-    return
+    res.status(404).send("The date is already booked for this hall!");
+    return;
   }
 
-  halls_booked.push(id)
+  halls_booked.push(id);
 
-  const obj = { date: bookedDate, halls_booked: halls_booked }
-  cal.push(obj)
+  const obj = { date: bookedDate, halls_booked: halls_booked };
+  cal.push(obj);
   //if it has push the hall_id onto the booked_halls array of the given date
   //if the hall_id is already present in the booked_halls array then return the date is already booked
 
@@ -132,7 +132,7 @@ async function bookHall(req, res) {
     // cancellation_buffer_days ||
     paid_amount_as_rent ||
     number_of_guests ||
-    event_type 
+    event_type
     // catering_amount_paid ||
     // total_amount_paid
   ) {
@@ -140,96 +140,99 @@ async function bookHall(req, res) {
       booking_id,
       user_id,
       hall_id: id,
-      booked_date:booked_date,
+      booked_date: booked_date,
       duration_hours,
       // cancellation_buffer_days,
-      paid_amount_as_rent:5000,
+      paid_amount_as_rent: 5000,
       number_of_guests,
       event_type,
       // catering_amount_paid,
       // total_amount_paid,
-    }
-    bookingRecords.push(newRecord)
+    };
+    bookingRecords.push(newRecord);
 
     //To clear the database
     // bookingRecords = [];
     // cal = [];
-    console.log(bookingRecords)
-    // const booking = await BookingsModel.updateOne({},{
-    //   bookingRecords: bookingRecords,
-    //   calendar: cal,
-    // })
-    res.status(200).json(booking)
-    return
+    console.log(bookingRecords);
+    const booking = await BookingsModel.updateOne(
+      {},
+      {
+        bookingRecords: bookingRecords,
+        calendar: cal,
+      }
+    );
+    res.status(200).json(booking);
+    return;
   }
 
-  res.status(404).send('Hall is booked!')
+  res.status(404).send("Hall is booked!");
 }
 
 async function getAvailableHalls(req, res) {
-  let date = req.params.date
-  const bookings = await BookingsModel.findOne({})
-  let cal = bookings.calendar
+  let date = req.params.date;
+  const bookings = await BookingsModel.findOne({});
+  let cal = bookings.calendar;
 
-  const bookedDate = new Date(date)
+  const bookedDate = new Date(date);
   let particularDate = cal.find(
     (dateObj) => dateObj.date.getDate() === bookedDate.getDate()
-  )
+  );
 
   // console.log(bookedDate, particularDate);
-  let halls_booked = []
-  if (particularDate) halls_booked = particularDate.halls_booked
+  let halls_booked = [];
+  if (particularDate) halls_booked = particularDate.halls_booked;
 
-  const halls = await HallModel.find({})
-  console.log('halls booked : ', halls_booked, halls)
-  let unbooked = halls.filter((hall) => !halls_booked.includes(hall.hall_id))
+  const halls = await HallModel.find({});
+  console.log("halls booked : ", halls_booked, halls);
+  let unbooked = halls.filter((hall) => !halls_booked.includes(hall.hall_id));
 
-  res.status(200).send(unbooked)
+  res.status(200).send(unbooked);
 }
 
 async function getProfile(req, res) {
-  let { id } = req.params
-  const user = await UserModel.findOne({ user_id: id })
+  let { id } = req.params;
+  const user = await UserModel.findOne({ user_id: id });
   if (!user) {
-    res.status(404).json({ message: 'User not found OR Invalid User' })
+    res.status(404).json({ message: "User not found OR Invalid User" });
   }
-  jwt.verify(req.token, 'secret', (err, data) => {
+  jwt.verify(req.token, "secret", (err, data) => {
     if (!err) {
-      res.status(200).json({ message: token, data: user })
-      return
+      res.status(200).json({ message: token, data: user });
+      return;
     } else {
-      res.status(404).json({ message: 'Token mismatch' })
-      return
+      res.status(404).json({ message: "Token mismatch" });
+      return;
     }
-  })
+  });
   //  else {
   //   res.status(200).json(user)
   // }
 }
 async function verify1(req) {
-  let f = 0
-  jwt.verify(req.token, 'secret', async (err, token) => {
+  let f = 0;
+  jwt.verify(req.token, "secret", async (err, token) => {
     if (err) {
-      f = 1
+      f = 1;
     }
-  })
-  return f
+  });
+  return f;
 }
 async function editProfile(req, res) {
-  let { id } = req.params
-  let err = await verify1(req)
+  let { id } = req.params;
+  let err = await verify1(req);
   if (err == 1) {
-    res.status(404).send({ message: 'Token mismatch' })
-    return
+    res.status(404).send({ message: "Token mismatch" });
+    return;
   }
-  const user = await UserModel.findOneAndUpdate({ user_id: id }, req.body)
+  const user = await UserModel.findOneAndUpdate({ user_id: id }, req.body);
   if (!user) {
-    res.status(404).json({ message: 'User not found' })
-    return
+    res.status(404).json({ message: "User not found" });
+    return;
   }
-  const user1 = await UserModel.findOne({ user_id: id })
-  res.status(200).json({ data: user1 })
-  return
+  const user1 = await UserModel.findOne({ user_id: id });
+  res.status(200).json({ data: user1 });
+  return;
 
   //  else {
   //   const user = await UserModel.findOne({ user_id: id })
@@ -238,43 +241,43 @@ async function editProfile(req, res) {
 }
 
 async function getBookingsCalendar(req, res) {
-  const calendar = BookingsModel.calendar
+  const calendar = BookingsModel.calendar;
   if (calendar) {
-    res.status(200).send(calendar)
-    return
+    res.status(200).send(calendar);
+    return;
   }
-  res.status(404).send('No calendar found!')
+  res.status(404).send("No calendar found!");
 }
 
 async function addReview(req, res) {
   // const hall_id = req.body
-  let err = await verify1(req)
+  let err = await verify1(req);
   if (err == 1) {
-    res.status(404).send({ message: 'Token mismatch' })
-    return
+    res.status(404).send({ message: "Token mismatch" });
+    return;
   }
-  const { hall_id, user_id, rating, comment } = req.body
+  const { hall_id, user_id, rating, comment } = req.body;
 
   if (rating < 0 || rating > 5) {
-    res.status(404).send('bad request!')
-    return
+    res.status(404).send("bad request!");
+    return;
   }
 
-  const reviewModel = await ReviewModel.findOne({ hall_id: hall_id })
-  let review_id = 0
+  const reviewModel = await ReviewModel.findOne({ hall_id: hall_id });
+  let review_id = 0;
   if (reviewModel != null) {
-    review_id = reviewModel.reviews.length + 1
+    review_id = reviewModel.reviews.length + 1;
   }
   if (reviewModel == null) {
-    review_id = 1
+    review_id = 1;
     const singleReview = {
       user_id,
       rating,
       comment,
       review_id,
-    }
-    revArray = []
-    revArray.push(singleReview)
+    };
+    revArray = [];
+    revArray.push(singleReview);
     // console.log(revArray)
 
     const hallreview = await ReviewModel.create(
@@ -283,10 +286,10 @@ async function addReview(req, res) {
         hall_id: hall_id,
         reviews: revArray,
       }
-    )
-    res.status(200).send('added review')
-    console.log(review_id)
-    return
+    );
+    res.status(200).send("added review");
+    console.log(review_id);
+    return;
   }
   // console.log(review_id_len)
   // console.log(reviewModel.reviews)
@@ -296,21 +299,23 @@ async function addReview(req, res) {
       rating,
       comment,
       review_id,
-    }
+    };
     // console.log(reviewModel.reviews)
-    const checkUser = reviewModel.reviews.find((rev) => rev.user_id === user_id)
+    const checkUser = reviewModel.reviews.find(
+      (rev) => rev.user_id === user_id
+    );
     // console.log(checkUser)
     if (!checkUser) {
       // console.log(singleReview)
-      const revArray = reviewModel.reviews
-      revArray.push(singleReview)
+      const revArray = reviewModel.reviews;
+      revArray.push(singleReview);
       // console.log(revArray)
 
       let avg = revArray.reduce(
         (accumulator, currentValue) => accumulator + currentValue.rating,
         0
-      )
-      avg = avg / revArray.length
+      );
+      avg = avg / revArray.length;
 
       const hallreview = await ReviewModel.updateOne(
         { hall_id: hall_id },
@@ -318,37 +323,37 @@ async function addReview(req, res) {
           hall_rating: avg,
           reviews: revArray,
         }
-      )
-      res.status(200).send('added review')
+      );
+      res.status(200).send("added review");
     } else {
-      res.status(404).send('you already added review')
+      res.status(404).send("you already added review");
     }
   } else {
-    res.status(404).send('hall not found')
+    res.status(404).send("hall not found");
   }
 }
 
 async function deleteReview(req, res) {
-  const { id } = req.params
-  let err = await verify1(req)
+  const { id } = req.params;
+  let err = await verify1(req);
   if (err == 1) {
-    res.status(404).send({ message: 'Token mismatch' })
-    return
+    res.status(404).send({ message: "Token mismatch" });
+    return;
   }
-  const { hall_id } = req.body
-  const reviewModel = await ReviewModel.findOne({ hall_id: hall_id })
+  const { hall_id } = req.body;
+  const reviewModel = await ReviewModel.findOne({ hall_id: hall_id });
 
-  let reviewArray = []
+  let reviewArray = [];
   if (reviewModel != null) {
-    reviewArray = reviewModel.reviews
+    reviewArray = reviewModel.reviews;
   }
 
-  const checkReview = reviewArray.find((r) => r.review_id == id)
+  const checkReview = reviewArray.find((r) => r.review_id == id);
   if (checkReview) {
     const updatedReviewArray = reviewArray.filter((r) => {
       // console.log(r.review_id, id)
-      return r.review_id != id
-    })
+      return r.review_id != id;
+    });
     // console.log(updatedReviewArray)
     if (updatedReviewArray) {
       await ReviewModel.updateOne(
@@ -356,12 +361,12 @@ async function deleteReview(req, res) {
         {
           reviews: updatedReviewArray,
         }
-      )
-      res.status(200).send('Deleted Review')
+      );
+      res.status(200).send("Deleted Review");
     }
   } else {
-    res.status(404).send('No Review found to delete')
-    return
+    res.status(404).send("No Review found to delete");
+    return;
   }
   // res.send('succ')
 }
@@ -369,75 +374,75 @@ async function deleteReview(req, res) {
 // const hall_id = req.body
 
 async function editReview(req, res) {
-  let err = await verify1(req)
+  let err = await verify1(req);
   if (err == 1) {
-    res.status(404).send({ message: 'Token mismatch' })
-    return
+    res.status(404).send({ message: "Token mismatch" });
+    return;
   }
-  const { id } = req.params
-  const { hall_id, comment, rating } = req.body
-  const reviewModel = await ReviewModel.findOne({ hall_id: hall_id })
+  const { id } = req.params;
+  const { hall_id, comment, rating } = req.body;
+  const reviewModel = await ReviewModel.findOne({ hall_id: hall_id });
 
-  let reviewArray = []
-  if (reviewModel) reviewArray = reviewModel.reviews
+  let reviewArray = [];
+  if (reviewModel) reviewArray = reviewModel.reviews;
   else {
-    res.status(404).send('Hall id incorrect!')
-    return
+    res.status(404).send("Hall id incorrect!");
+    return;
   }
 
-  let rev = reviewArray.find((r) => r.review_id === id)
+  let rev = reviewArray.find((r) => r.review_id === id);
   if (!rev) {
-    res.status(404).send('review id incorrect!')
-    return
+    res.status(404).send("review id incorrect!");
+    return;
   }
   if (rating < 0 || rating > 5) {
-    res.status(404).send('bad request!')
-    return
+    res.status(404).send("bad request!");
+    return;
   }
 
-  let ind = reviewArray.indexOf(rev)
-  if (comment) rev.comment = comment
-  if (rating) rev.rating = rating
-  reviewArray[ind] = rev
+  let ind = reviewArray.indexOf(rev);
+  if (comment) rev.comment = comment;
+  if (rating) rev.rating = rating;
+  reviewArray[ind] = rev;
 
-  console.log(id)
+  console.log(id);
   await ReviewModel.updateOne(
     { hall_id: hall_id },
     {
       reviews: reviewArray,
     }
-  )
+  );
 
-  res.send(reviewArray)
+  res.send(reviewArray);
 }
 
 async function getReview(req, res) {
-  let err = await verify1(req)
+  let err = await verify1(req);
   if (err == 1) {
-    res.status(404).send({ message: 'Token mismatch' })
-    return
+    res.status(404).send({ message: "Token mismatch" });
+    return;
   }
-  const hall_id = req.params.id
-  const reviewModel = await ReviewModel.findOne({ hall_id: hall_id })
+  const hall_id = req.params.id;
+  const reviewModel = await ReviewModel.findOne({ hall_id: hall_id });
   if (!reviewModel) {
-    res.status(400).send('Enter correct Hall id')
-    return
+    res.status(400).send("Enter correct Hall id");
+    return;
   }
-  console.log(reviewModel)
-  let avg_rating = reviewModel.hall_rating
-  console.log(avg_rating)
-  let reviewArray = []
-  if (reviewModel) reviewArray = reviewModel.reviews
+  console.log(reviewModel);
+  let avg_rating = reviewModel.hall_rating;
+  console.log(avg_rating);
+  let reviewArray = [];
+  if (reviewModel) reviewArray = reviewModel.reviews;
   else {
-    res.status(404).send('Hall id incorrect!')
-    return
+    res.status(404).send("Hall id incorrect!");
+    return;
   }
   let hall_specific_review = {
     avg_rating: avg_rating,
     reviews: reviewArray,
-  }
+  };
 
-  res.status(200).send(hall_specific_review)
+  res.status(200).send(hall_specific_review);
 }
 
 module.exports = {
@@ -452,4 +457,4 @@ module.exports = {
   editReview,
   getReview,
   verify1,
-}
+};
