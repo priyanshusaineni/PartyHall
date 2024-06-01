8577c179684bed6daa7960d496a2298bbc0d1cfd
shivaramakrishnamarepalli <marepallishivaramakrishna@gmail.com>
2024-06-01 20:51:56 +0530

    adin login fixed

--- Controllers/auth.controller.js
+++ Controllers/auth.controller.js
@@ -1,15 +1,15 @@
-const AdminModel = require('../Models/admin.model')
-const UserModel = require('../Models/user.model')
-const jwt = require('jsonwebtoken')
+const AdminModel = require("../Models/admin.model");
+const UserModel = require("../Models/user.model");
+const jwt = require("jsonwebtoken");
 //auth controller
 async function isSuperAdmin() {
   // token based auth is required
-  return true
+  return true;
 }
 
 async function isAdmin() {
   // token based auth is required
-  return true
+  return true;
 }
 
 async function addNewUser(req, res) {
@@ -20,13 +20,13 @@ async function addNewUser(req, res) {
     user_password,
     user_age,
     user_mobile_no,
-  } = req.body
+  } = req.body;
 
-  const user = await UserModel.findOne({ user_id: user_id })
+  const user = await UserModel.findOne({ user_id: user_id });
   if (user) {
     res
       .status(404)
-      .json({ message: 'User already exists kindly please login !' })
+      .json({ message: "User already exists kindly please login !" });
   } else {
     if (user_id || user_name || user_email || user_password || user_mobile_no) {
       const user = await UserModel.create({
@@ -36,41 +36,41 @@ async function addNewUser(req, res) {
         user_password,
         user_age,
         user_mobile_no,
-      })
-      res.status(201).json(user)
-      return
+      });
+      res.status(201).json(user);
+      return;
     } else {
-      res.status(400).json({ message: 'Please enter all details' })
-      return
+      res.status(400).json({ message: "Please enter all details" });
+      return;
     }
   }
 }
 async function userLogin(req, res) {
-  console.log("user login")
-  const { user_id, user_password } = req.body
-  const user = await UserModel.findOne({ user_id, user_password })
-  if(!user) {
-    res.status(404).send("no user found!")
-    return
+  console.log("user login");
+  const { user_id, user_password } = req.body;
+  const user = await UserModel.findOne({ user_id, user_password });
+  if (!user) {
+    res.status(404).send("no user found!");
+    return;
   }
-  console.log(user_id)
-  jwt.sign({ user_id }, 'secret', (err, token) => {
-    res.status(200).send({ token })
-  })
+  console.log(user_id);
+  jwt.sign({ user_id }, "secret", (err, token) => {
+    res.status(200).send({ token });
+  });
 }
 
 function verifyToken(req, res, next) {
   if (!req.headers.authorization) {
-    res.status(404).send('Please Login no token present')
-    return
+    res.status(404).send("Please Login no token present");
+    return;
   }
-  token = req.headers.authorization.split(' ')[1]
-  req.token = token
+  token = req.headers.authorization.split(" ")[1];
+  req.token = token;
   if (token) {
     // console.log(token)
-    next()
+    next();
   } else {
-    res.status(404).send('No token found')
+    res.status(404).send("No token found");
   }
   // token=
   // req.token=token;
@@ -78,13 +78,19 @@ function verifyToken(req, res, next) {
 }
 
 async function addNewAdmin(req, res) {
-  const { admin_id, admin_name, admin_email, admin_password, admin_mobile_no } =
-    req.body
+  const {
+    admin_id,
+    admin_name,
+    admin_email,
+    admin_password,
+    admin_age,
+    admin_mobile_no,
+  } = req.body;
 
-  const admin = await AdminModel.findOne({ admin_id: admin_id })
+  const admin = await AdminModel.findOne({ admin_id: admin_id });
   if (admin) {
-    res.status(404).json({ message: 'Admin already exists please login' })
-    return
+    res.status(404).json({ message: "Admin already exists please login" });
+    return;
   } else {
     if (
       admin_id ||
@@ -99,21 +105,30 @@ async function addNewAdmin(req, res) {
         admin_email,
         admin_password,
         admin_mobile_no,
-      })
-      res.status(201).json(admin)
-      return
+        admin_age,
+      });
+      res.status(201).json(admin);
+      return;
     } else {
-      res.status(404).json({ message: 'Please enter all the required details' })
+      res
+        .status(404)
+        .json({ message: "Please enter all the required details" });
     }
   }
 }
 
 async function adminLogin(req, res) {
-  const { admin_email } = req.body
-  console.log(admin_email)
-  jwt.sign({ admin_email }, 'secret', (err, token) => {
-    res.send({ token })
-  })
+  console.log("admin login");
+  const { admin_id, admin_password } = req.body;
+  const admin = await AdminModel.findOne({ admin_id, admin_password });
+  if (!admin) {
+    res.status(404).send("no admin found!");
+    return;
+  }
+  console.log(admin_id);
+  jwt.sign({ admin_id }, "secret", (err, token) => {
+    res.status(200).send({ token });
+  });
 }
 
 // async function addNewSuperAdmin() {}
@@ -130,4 +145,4 @@ module.exports = {
   verifyToken,
   // addNewSuperAdmin,
   superAdminLogin,
-}
+};
